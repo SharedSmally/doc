4bbddc3aa3329b46f4dd507700e00e2c617c4436
SharedSmally <SharedSmally@gmail.com>
2017-10-11 17:23:00 -0400

    Update cxxmake.mk

--- makefile/cxxmake.mk
+++ makefile/cxxmake.mk
@@ -50,3 +50,6 @@ print:
 	@echo TESTS: ${TESTS}
 	@echo CLASSES: ${CLASSES}
 ###########################################
+#gcc -fPIC -g -c -Wall a.c
+#gcc -shared -Wl,-soname,libmystuff.so.1 -o libmystuff.so.1.0.1 a.o b.o -lc
+    
