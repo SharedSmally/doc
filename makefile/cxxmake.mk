15d2b780d2a732d8cbc0cbea380ae76efa8747ae
SharedSmally <SharedSmally@gmail.com>
2017-10-11 17:21:30 -0400

    Update cxxmake.mk

--- makefile/cxxmake.mk
+++ makefile/cxxmake.mk
@@ -1,4 +1,5 @@
-CPPFLAGS += -I./ -std=c++11 -O2
+###########################################
+CPPFLAGS += -I./ -std=c++11
 
 CC_SRCS=$(wildcard *.cc)
 TEST_SRCS=$(wildcard *Test.cc)
@@ -7,17 +8,45 @@ OBJ_SRCS=$(filter-out $(TEST_SRCS),$(CC_SRCS))
 OBJS=$(OBJ_SRCS:.cc=.o)
 TESTS=$(TEST_SRCS:.cc=)
 
+debug: DEFINE += -DDEBUG
+debug: CPPFLAGS += -g
+
+opt:CPPFLAGS += -O2
+
+.PHONY : clean
+
+#CPPFLAGS= -fPIC -g
+#LDFLAGS= -shared
+
+###########################
+-include lib.mk
+LIBNAME=$(NAME)-${VERSION}.so
+###########################################
 main: build
 
 build: ${OBJS} ${TEST_TARGETS}
 
-%Test:%Test.cc
-        g++ -o $@ $< ${OBJS} -std=c++11 -lpthread -I./
+%Test:%Test.cc 
+	${CXX} -o $@ $< ${OBJS} -std=c++11 -lpthread -I./
 
+#%.o:%.cc
+#	g++ -c $@ $< ${OBJS} -std=c++11 -lpthread -I./
+
+lib:${OBJS}
+	${CXX} -shared -o ${LIBNAME} ${OBJS}
+	
 clean:
-        ${RM} -rf ${OBJS} *Test
+	${RM} -rf ${OBJS} *Test
 
+init:
+	@export NS=com.cpw.core
+	@for cls in ${CLASSES}; do \
+    	class $$cls ; \
+	done
+	
 print:
-        @echo SRCS: ${CC_SRCS}
-        @echo OBJS: ${OBJS}
-        @echo TESTS: ${TESTS}        
+	@echo SRCS: ${CC_SRCS}
+	@echo OBJS: ${OBJS}
+	@echo TESTS: ${TESTS}
+	@echo CLASSES: ${CLASSES}
+###########################################
