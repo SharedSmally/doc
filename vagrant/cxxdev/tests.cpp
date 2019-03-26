ef7529eaa8fb1f11369f3b7949762beac079f588
SharedSmally <SharedSmally@gmail.com>
2019-03-26 14:04:51 -0400

    Create tests.cpp

---
+++ vagrant/cxxdev/tests.cpp
@@ -0,0 +1,28 @@
+// tests.cpp
+#include <math.h>
+
+double squareRoot(double r0)
+{
+    if (r0<0)  return -1.0;
+
+    return sqrt(r0);
+}
+
+#include <gtest/gtest.h>
+
+TEST(SquareRootTest, PositiveNos) {
+    ASSERT_EQ(6, squareRoot(36.0));
+    ASSERT_EQ(18.0, squareRoot(324.0));
+    ASSERT_EQ(25.4, squareRoot(645.16));
+    ASSERT_EQ(0, squareRoot(0.0));
+}
+
+TEST(SquareRootTest, NegativeNos) {
+    ASSERT_EQ(-1.0, squareRoot(-15.0));
+    ASSERT_EQ(-1.0, squareRoot(-0.2));
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
