3a5b0338826e6f8468e22695904293256be1d2b9
SharedSmally <SharedSmally@gmail.com>
2019-03-29 22:36:51 -0400

    Create jenkins.sh

---
+++ vagrant/devops/jenkins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+#
+# https://linuxize.com/post/how-to-install-jenkins-on-centos-7/
+#
+# Install JDK
+#sudo yum install java-1.8.0-openjdk-devel
+
+#
+# Enable the Jenkins repository
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+#install the latest stable version of Jenkins:
+sudo yum install jenkins
+
+#start the Jenkins service with:
+sudo systemctl start jenkins
+
+#check whether it started successfully run:
+systemctl status jenkins
