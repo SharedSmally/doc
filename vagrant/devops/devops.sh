15d82f6bf1317a35b165a89cdd7e6fb028fa9ed8
SharedSmally <SharedSmally@gmail.com>
2019-04-01 08:24:43 -0400

    Update devops.sh

--- vagrant/devops/devops.sh
+++ vagrant/devops/devops.sh
@@ -6,6 +6,11 @@ function update {
    sudo yum install -y wget
    sudo yum install -y git
    sudo yum install -y java-1.8.0-openjdk-devel
+   
+   #sudo vi /etc/profile
+   #export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
+   #export JRE_HOME=/usr/lib/jvm/jre
+   #source /etc/profile
 }
 
 #######################################
