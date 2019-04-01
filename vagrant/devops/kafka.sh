fd678fea845ccdcf85873d086db7b16137747c04
SharedSmally <SharedSmally@gmail.com>
2019-04-01 10:06:52 -0400

    Update kafka.sh

--- vagrant/devops/kafka.sh
+++ vagrant/devops/kafka.sh
@@ -9,6 +9,13 @@ export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
 export JRE_HOME=/usr/lib/jvm/jre
 source /etc/profile
 
+# download kafka
+cd ~
+wget http://www-us.apache.org/dist/kafka/2.2.0/kafka_2.12-2.2.0.tgz
+
+sudp tar -xvf kafka_2.12-2.2.0.tgz -C /opt
+sudo ln -s /opt/kafka_2.12-2.2.0/ /opt/kafka
+
 # create kafka user
 sudo adduser kafka
 sudo chown -R kafka:kafka /opt/kafka
@@ -16,13 +23,6 @@ sudo chown -R kafka:kafka /opt/kafka
 #sudo useradd kafka -m
 #sudo passwd kafka
 #sudo usermod -aG wheel kafka
-
-# download kafka
-cd ~
-wget http://www-us.apache.org/dist/kafka/2.2.0/kafka_2.12-2.2.0.tgz
-
-sudp tar -xvf kafka_2.12-2.2.0.tgz -C /opt
-sudo ln -s /opt/kafka_2.12-2.2.0/ /opt/kafka
 
 # run zookeeper as a service
 sudo vi /etc/systemd/system/zookeeper.service
@@ -34,8 +34,8 @@ After=network.target remote-fs.target
 [Service]
 Type=simple
 User=kafka
-ExecStart=/opt/kafka/kafka/bin/zookeeper-server-start.sh /opt/kafka/kafka/config/zookeeper.properties
-ExecStop=/opt/kafka/kafka/bin/zookeeper-server-stop.sh
+ExecStart=/opt/kafka/bin/zookeeper-server-start.sh /opt/kafka/config/zookeeper.properties
+ExecStop=/opt/kafka/bin/zookeeper-server-stop.sh
 Restart=on-abnormal
 
 [Install]
@@ -51,23 +51,38 @@ After=zookeeper.service
 [Service]
 Type=simple
 User=kafka
-ExecStart=/bin/sh -c '/opt/kafka/kafka/bin/kafka-server-start.sh /opt/kafka/kafka/config/server.properties > /opt/kafka/kafka/kafka.log 2>&1'
-ExecStop=/opt/kafka/kafka/bin/kafka-server-stop.sh
+ExecStart=/bin/sh -c '/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties > /tmp/kafka.log 2>&1'
+ExecStop=/opt/kafka/bin/kafka-server-stop.sh
 Restart=on-abnormal
 
 [Install]
 WantedBy=multi-user.target
 ```
+# Modify the configuration of your Kafka server
+sudo vi /opt/kafka/bin/kafka-server-start.sh
+  export KAFKA_HEAP_OPTS="-Xmx256M -Xms128M"
+  
+# start kafka service
+sudo systemctl start kafka
+# check kafka status
+sudo journalctl -u kafka
+# enable kafka service
+sudo systemctl enable kafka
 
+#test kafka
+# /opt/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic TutorialTopic
+# echo "Hello, World" | /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic TutorialTopic > /dev/null
+# /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic TutorialTopic --from-beginning
+
+
+#######################################
 # Get into the Kafka directory
 cd /opt/kafka
 
 # Start the Zookeeper server
 bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
 
-# Modify the configuration of your Kafka server
-vi bin/kafka-server-start.sh
-  export KAFKA_HEAP_OPTS="-Xmx256M -Xms128M"
+
 
 #Start the Kafka server
 bin/kafka-server-start.sh config/server.properties
