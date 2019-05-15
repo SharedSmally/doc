7e9b607d0ed10e5cafb967d6b8c476fd0ce87736
SharedSmally <SharedSmally@gmail.com>
2019-05-15 16:38:48 -0400

    Create PollMux.h

---
+++ cxx/net/PollMux.h
@@ -0,0 +1,19 @@
+#ifndef POLLMUX_H
+#define POLLMUX_H
+
+#include <Multiplexer.h>
+
+class PollMux
+  : public Multiplexer
+{
+public:
+    PollMux();
+    ~PollMux();
+
+protected:
+
+private:
+
+};
+
+#endif /* end of POLLMUX_H */
