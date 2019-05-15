4ed7714af6cff7e1c5728eae422ff46a5897ea9f
SharedSmally <SharedSmally@gmail.com>
2019-05-15 16:38:24 -0400

    Create SelectMux.h

---
+++ cxx/net/SelectMux.h
@@ -0,0 +1,19 @@
+#ifndef SELECTMUX_H
+#define SELECTMUX_H
+
+#include <Multiplexer.h>
+
+class SelectMux
+  : public Multiplexer
+{
+public:
+    SelectMux();
+    ~SelectMux();
+
+protected:
+
+private:
+
+};
+
+#endif /* end of SELECTMUX_H */
