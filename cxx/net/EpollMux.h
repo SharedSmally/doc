adc4ff78934dc74581a3c36d28d08b047c962422
SharedSmally <SharedSmally@gmail.com>
2019-05-16 16:33:41 -0400

    Update EpollMux.h

--- cxx/net/EpollMux.h
+++ cxx/net/EpollMux.h
@@ -5,7 +5,7 @@
 #include <sys/epoll.h>
 
 class EpollMux
-  : public Multiplexer
+  : public Multiplexer, public IOEventsHandler
 {
 public:
     EpollMux();
@@ -13,18 +13,20 @@ public:
 
     virtual bool add(FdObjPtr & ptr);
     virtual bool remove(FdObjPtr & ptr);
+    virtual bool onEvents(FdObjPtr & obj);
+
+    virtual IOEventsHandler & getHandler() { return *this; }
 
 protected:
     virtual void monitorTask();
-    virtual void ioTask();
-
-    void setEvent(epoll_event & event, FdObjPtr & );
-    uint32_t getEvents(uint32_t revents);
 
     virtual bool notify(); //notify new set of fds to be minitored
     virtual bool addNotify(bool addit=false);
 
     bool monitor(FdObjPtr & ptr);
+
+    void setEvent(epoll_event & event, FdObjPtr & );
+    uint32_t getEvents(uint32_t revents);
 
 private:
     int epollfd_;
