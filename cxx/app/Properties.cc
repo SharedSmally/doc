cb24dd3f242279362bedb1a26d95589a615f1470
SharedSmally <SharedSmally@gmail.com>
2019-05-31 16:16:17 -0400

    Create Properties.cc

---
+++ cxx/app/Properties.cc
@@ -0,0 +1,139 @@
+#include "Properties.h"
+#include "Utils.h"
+
+#include <fstream>
+
+const String Properties::EMPTY_STRING("");
+
+bool Properties::hasProperty(const String & key) const
+{
+	return properties_.find(key) != properties_.end();
+}
+
+const String & Properties::getProperty(const String & key) const
+{
+	return getProperty(key, EMPTY_STRING);
+}
+
+const String & Properties::getProperty(const String & key, const String & defaultValue) const
+{
+	auto iter = properties_.find(key);
+	return properties_.find(key) == properties_.end() ? defaultValue : iter->second;
+}
+
+void Properties::setProperty(const String & key, const String & value)
+{
+	auto iter = properties_.insert(std::make_pair(key, value));
+	if (iter.second) { // new one
+		names_.push_back(key);
+	} else {  // exist one
+		properties_[key] = value;
+	}
+}
+
+void Properties::output(std::ostream & out) const
+{
+	out << "[ " << name_ << " ]\n";
+	for (auto & name : names_) {
+		out << name << " = " << getProperty(name) << "\n";
+	}
+	out << "\n";
+}
+
+PropertiesGroup::~PropertiesGroup()
+{
+	for (auto iter : groups_) {
+		delete iter.second;
+		iter.second = nullptr;
+	}
+	groups_.clear();
+	names_.clear();
+}
+
+bool PropertiesGroup::hasGroup(const String & name) const
+{
+	return groups_.find(name) != groups_.end();
+}
+const Properties * PropertiesGroup::getGroup(const String & name) const
+{
+	auto iter = groups_.find(name);
+	return groups_.find(name) == groups_.end() ? nullptr : iter->second;
+}
+Properties * PropertiesGroup::getGroup(const String & name)
+{
+	auto iter = groups_.find(name);
+	if (iter != groups_.end() ) return iter->second;
+
+	Properties * group = new Properties(name);
+	names_.push_back(name);
+	groups_.insert(std::make_pair(name, group));
+	return group;
+}
+
+void PropertiesGroup::removeGroup(const String & name)
+{
+	auto iter = groups_.find(name);
+	if (iter == groups_.end() ) return;
+
+	groups_.erase(iter);
+	delete iter->second;
+	names_.remove(name);
+}
+
+bool PropertiesGroup::load(const String & cfgfile)
+{
+    std::ifstream input( cfgfile.c_str());
+    if (!input)
+       return false;
+
+    String gname;
+    Properties * props;
+
+    for( std::string line; getline( input, line ); )
+    {
+        trim(line);
+
+        if (line.empty()) continue;
+
+        if (line[0]=='[')  // group
+        {
+            gname=::group_name(line);
+            props = getGroup(gname);
+        }
+        else if (props != nullptr)
+        {
+            auto ind = line.find_first_of('=');
+            String name=line.substr(0,ind);
+            String value=line.substr(ind+1);
+            props->setProperty(trim(name), trim(value) );
+        }
+        /*else {
+            std::cout << "Properties if null for name=" << gname << std::endl;
+        }
+        std::cout << "read: {" << line << "}" << std::endl;
+        */
+    }
+
+    return true;
+}
+
+bool PropertiesGroup::store(const String & cfgfile) const
+{
+    std::ofstream fout( cfgfile.c_str());
+    if (!fout)
+       return false;
+
+    output(fout);
+
+    return fout.good();
+}
+
+void PropertiesGroup::output(std::ostream & out) const
+{
+    for (auto & name : names_)
+    {
+        const Properties * props = getGroup(name);
+        if (props != nullptr)
+            props->output(out);
+    }
+}
