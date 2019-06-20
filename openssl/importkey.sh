35652a90dbc5a7e2aab2c4ff22f39706d7d57237
SharedSmally <SharedSmally@gmail.com>
2019-06-20 16:59:30 -0400

    import openssl key/root cert to keystore.

---
+++ openssl/importkey.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    echo "USAGE: ./importkey.sh server.pem root.pem"
+    exit 1
+fi
+
+echo "generating keystore from server certificate $1 and client root certificate $2..."
+
+rm -f keystore.jks
+
+export OPENSSL_CONF=/etc/pki/tls/openssl.cnf
+
+openssl pkcs12 -export -out keystore.p12 -inkey $1 -in $1 -passout pass:password
+keytool -importkeystore -destkeystore keystore.jks -srcstoretype PKCS12 -srckeystore keystore.p12 -srcstorepass password -deststorepass password
+keytool -importcert -noprompt -srcstorepass password -deststorepass password -alias ca -keystore keystore.jks -trustcacerts -file $2
