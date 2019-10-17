105bc8cc9093a33d586b52e0b64aaea73de0e0a8
SharedSmally <SharedSmally@gmail.com>
2019-10-17 17:24:45 -0400

    Create Multiplexer.h

---
+++ cxx/net1/Multiplexer.h
@@ -0,0 +1,60 @@
+#ifndef MULTIPLEXER_H
+#define MULTIPLEXER_H
+
+#include <thread>
+#include <vector>
+#include <mutex>
+
+#include <Channel.h>
+
+class Notifier{
+	const static uint64_t ONE;
+
+public:
+	Notifier(uint32_t initval=0, bool semaphore=false,
+			bool cloexec=true, bool nonblock=true);
+	virtual ~Notifier();
+	int fd() { return efd_; }
+	bool notify();
+
+protected:
+	bool onNotified(uint64_t & value);
+
+protected:
+	int efd_;
+	uint32_t events_;
+};
+
+class Multiplexer : public Notifier
+{
+	const static int STEP_BIT_NUM;
+public:
+	typedef std::mutex mutex_type;
+	typedef std::lock_guard<mutex_type>  guard_type;
+	typedef std::unique_lock<mutex_type> lock_type;
+
+	Multiplexer(uint32_t initSize=1024) ;
+	virtual ~Multiplexer() { }
+
+	bool start();
+	void stop();
+	bool isRunning() const { return running_; }
+
+	virtual bool manage(ChannelPtr & channel) ;
+	virtual bool unmanage(ChannelPtr & channel) ;
+	//ChannelListener & getListener() = 0;
+
+protected:
+	// one thread to monitor the fds
+	virtual void _run() = 0;
+	virtual bool _join(ChannelPtr & channel, bool modify) = 0;
+	virtual bool _leave(ChannelPtr & channel) =0;
+
+protected:
+	mutable mutex_type mtx_;
+	volatile bool running_;
+	std::vector<ChannelPtr> channels_;
+	std::shared_ptr<std::thread> ioThread_;
+};
+
+#endif
