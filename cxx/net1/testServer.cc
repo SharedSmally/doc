d8b06861997c59bc65280e38df4318bed7273c70
SharedSmally <SharedSmally@gmail.com>
2019-10-17 22:30:16 -0400

    Update testServer.cc

--- cxx/net1/testServer.cc
+++ cxx/net1/testServer.cc
@@ -8,9 +8,8 @@ using namespace std;
 
 int main()
 {
-	IPv4Address addr("10.32.119.152"); // no port
-	IPv4Address svrAddr("10.32.119.152", 9999);
-	auto channel = clientChannel(addr, svrAddr);
+	IPv4Address addr("10.32.119.152", 9999);
+	auto channel = serverChannel(addr);
 
 	EpollMux epoll;
 	epoll.start();
