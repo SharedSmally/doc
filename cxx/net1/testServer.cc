12654d7f41255e51b620d4495d6314af26c36869
SharedSmally <SharedSmally@gmail.com>
2019-10-18 14:39:08 -0400

    Update testServer.cc

--- cxx/net1/testServer.cc
+++ cxx/net1/testServer.cc
@@ -6,15 +6,62 @@ using namespace std;
 
 #include <unistd.h>
 
+class MyChannelListener : public ChannelListener
+{
+public:
+	MyChannelListener(const IPv4Address & svcAddr)
+	: svcAddr_(svcAddr) {}
+    virtual ~MyChannelListener() {}
+
+protected:
+    virtual int _onRead(ChannelPtr & channel, bool prio)
+    {
+    	if (channel) {
+    		INFO("MyChannelListener onRead: id="<<channel->id());
+    	} else {
+    		INFO("MyChannelListener onRead: channel is null");
+    	}
+    }
+    virtual int _onWrite(ChannelPtr & channel)
+    {
+    	if (channel) {
+    		INFO("MyChannelListener _onWrite: id="<<channel->id());
+    	} else {
+    		INFO("MyChannelListener _onWrite: channel is null");
+    	}
+    }
+    virtual int _onHup(ChannelPtr & channel)
+    {
+    	if (channel) {
+    		INFO("MyChannelListener _onHup: id="<<channel->id());
+    	} else {
+    		INFO("MyChannelListener _onHup: channel is null");
+    	}
+    }
+    virtual int _onError(ChannelPtr & channel)
+    {
+    	if (channel) {
+    		INFO("MyChannelListener _onError: id="<<channel->id());
+    	} else {
+    		INFO("MyChannelListener _onError: channel is null");
+    	}
+    }
+
+protected:
+    IPv4Address svcAddr_;
+};
+
+
+
 int main()
 {
-	IPv4Address addr("10.32.119.152", 9999);
-	auto channel = serverChannel(addr);
+    IPv4Address addr("10.32.119.152", 9999);
+    auto channel = serverChannel(addr);
+    INFO("server addr="<<addr <<"; fd=" << channel->fd());
+    MyChannelListener listener(addr);
 
-	EpollMux epoll;
-	epoll.start();
-	epoll.manage(channel);
-	sleep(10);
-	epoll.stop();
-
+    EpollMux epoll(listener);
+    epoll.start();
+    epoll.manage(channel);
+    epoll.join();
 }
