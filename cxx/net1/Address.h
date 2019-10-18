e6368570e359693620e3126d568b8bc2e0c571f8
SharedSmally <SharedSmally@gmail.com>
2019-10-18 14:42:42 -0400

    Update Address.h

--- cxx/net1/Address.h
+++ cxx/net1/Address.h
@@ -1,22 +1,36 @@
-#ifndef ADDRESS_H
-#define ADDRESS_H
+#ifndef CPW_NET_ADDRESS_H
+#define CPW_NET_ADDRESS_H
 
 #include <string>
+#include <iostream>
+#include <sstream>
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 class Address {
+    friend std::ostream & operator<<(std::ostream & out, const Address & addr)
+    {
+        addr.output(out); return out;
+    }
 public:
-	virtual ~Address() {}
-	virtual int domain() const = 0;
-	
-	virtual struct sockaddr * addr() = 0;
-	virtual const struct sockaddr * addr() const = 0;
-	
-	virtual socklen_t length() const = 0;
-	virtual socklen_t size() const { return length(); }
-	virtual socklen_t maxLength() const  { return length(); }
+    virtual ~Address() {}
+
+    virtual int domain() const = 0;
+
+    virtual struct sockaddr * addr() = 0;
+    virtual const struct sockaddr * addr() const = 0;
+
+    virtual socklen_t length() const = 0;
+    virtual socklen_t size() const { return length(); }
+    virtual socklen_t maxLength() const  { return length(); }
+
+    virtual void output(std::ostream & out) const = 0;
+    std::string toString() const {
+        std::ostringstream oss;
+        output(oss);
+        return oss.str();
+    }
 };
 
 /*
@@ -32,33 +46,33 @@ public:
  */
 class IPv4Address : public Address {
 public:
-	IPv4Address(uint16_t p=0);
-	IPv4Address(struct sockaddr_in && addr);
-	IPv4Address(const struct sockaddr_in & addr);
-	IPv4Address(const IPv4Address & addr);
-	IPv4Address(IPv4Address && addr);
-	IPv4Address(uint32_t i, uint16_t p=0);
-	IPv4Address(const std::string & i, uint16_t p=0) ;
-	~IPv4Address() {}
+    IPv4Address(uint16_t p=0);
+    IPv4Address(struct sockaddr_in && addr);
+    IPv4Address(const struct sockaddr_in & addr);
+    IPv4Address(const IPv4Address & addr);
+    IPv4Address(IPv4Address && addr);
+    IPv4Address(uint32_t i, uint16_t p=0);
+    IPv4Address(const std::string & i, uint16_t p=0) ;
+    ~IPv4Address() {}
 
-	virtual struct sockaddr * addr() { return (struct sockaddr *)(&addr_); }
-	virtual const struct sockaddr * addr() const { return (struct sockaddr *)(&addr_); }
-	virtual socklen_t length() const     { return sizeof(addr_); }
-	virtual socklen_t maxLength() const  { return sizeof(addr_); }
+    virtual int domain() const { return AF_INET; }
 
-	virtual int domain() const { return AF_INET; }
+    virtual struct sockaddr * addr() { return (struct sockaddr *)(&addr_); }
+    virtual const struct sockaddr * addr() const { return (struct sockaddr *)(&addr_); }
+    virtual socklen_t length() const     { return sizeof(addr_); }
 
-	void ip(uint32_t p) { addr_.sin_addr.s_addr = htonl(p);}
-	uint32_t ipInt() const { return ntohl(addr_.sin_addr.s_addr); }
-	bool ip(const std::string & p);
-	std::string ip() const;
+    void ip(uint32_t p) { addr_.sin_addr.s_addr = htonl(p);}
+    uint32_t ipInt() const { return ntohl(addr_.sin_addr.s_addr); }
+    bool ip(const std::string & p);
+    std::string ip() const;
 
-	void port(uint16_t p) { addr_.sin_port = htons(p); }
-	uint16_t port() const { return ntohs(addr_.sin_port); }
+    void port(uint16_t p) { addr_.sin_port = htons(p); }
+    uint16_t port() const { return ntohs(addr_.sin_port); }
 
+    virtual void output(std::ostream & out) const;
 protected:
-	void _init();
-	struct sockaddr_in addr_;
+    void _init();
+    struct sockaddr_in addr_;
 };
 
 #endif
