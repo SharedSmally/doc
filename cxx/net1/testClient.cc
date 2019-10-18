7171ab9b17878c4dfd34ec9dc3e45ddd121db67e
SharedSmally <SharedSmally@gmail.com>
2019-10-17 22:29:32 -0400

    Update testClient.cc

--- cxx/net1/testClient.cc
+++ cxx/net1/testClient.cc
@@ -1,11 +1,21 @@
+#include <Utils.h>
 #include <EpollMux.h>
 
 #include <iostream>
 using namespace std;
 
+#include <unistd.h>
+
 int main()
 {
+	IPv4Address addr("10.32.119.152"); // no port
+	IPv4Address svrAddr("10.32.119.152", 9999);
+	auto channel = clientChannel(addr, svrAddr);
+
 	EpollMux epoll;
-	//ChannelPtr client = std::make_shared<CSocket>();
+	epoll.start();
+	epoll.manage(channel);
+	sleep(10);
+	epoll.stop();
 
 }
