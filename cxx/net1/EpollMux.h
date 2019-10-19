85e67d6c53a4813ce58db721a28a7273863a4c23
SharedSmally <SharedSmally@gmail.com>
2019-10-18 23:10:31 -0400

    Update EpollMux.h

--- cxx/net1/EpollMux.h
+++ cxx/net1/EpollMux.h
@@ -21,6 +21,10 @@ protected:
     // epoll events to channel events
     uint32_t getChannelEvents(uint32_t events);
 
+    // conversion between epoll.data.u64 and id + fd
+    uint64_t toEpollU64(Channel::id_type id, uint32_t fd);
+    void fromEpollU64(Channel::id_type & id, uint32_t & fd, uint64_t evtu64);
+
 protected:
     int epollfd_;
 };
