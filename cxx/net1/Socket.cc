02bf0d53ddb09116b5efd7982292c8ff2f570ee5
SharedSmally <SharedSmally@gmail.com>
2019-10-18 23:13:52 -0400

    Update Socket.cc

--- cxx/net1/Socket.cc
+++ cxx/net1/Socket.cc
@@ -1,6 +1,9 @@
 #include <Socket.h>
+#include <Macro.h>
 
 #include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/sctp.h>
 #include <sys/socket.h>
@@ -12,11 +15,18 @@ Socket::Socket(int domain, int type, int protocol)
 {
 	fd_ = socket(domain, type, protocol);
 	//check
+	_nonblock();
 }
 Socket::~Socket()
 {
 	if (fd_ > BAD_FD)
 		::close(fd_);
+}
+int Socket::_nonblock()
+{
+	//make socket nonblocking
+	int flags = fcntl(fd_, F_GETFL, 0);
+	fcntl(fd_, F_SETFL, flags | O_NONBLOCK);
 }
 
 int Socket::_bind(const Address & addr)
@@ -51,62 +61,54 @@ CSocket::CSocket(int domain, bool sctp)
 
 //ssize_t send(int socket, const void *buffer, size_t length, int flags); flags: MSG_EOR, MSG_OOB
 //ssize_t recv(int socket, void *buffer, size_t length, int flags); flags: MSG_PEEK, MSG_OOB, MSG_WAITALL
-ssize_t CSocket::send(const Buffer & buffer, int flags)
+ssize_t CSocket::send(const char * data, size_t len, int flags)
 {
-	ssize_t total(0);
-    size_t left = buffer.size();
-    int ret (0);
-    char *ptr = (char *)buffer.data();
+    ssize_t total(0);
+    int ret (0), ecode(0);
+    char *ptr = (char *)data;
 
-    while (left != 0 ) {
-        int ret = ::send(fd_, ptr, left, flags);
-
-        if (ret == 0 || ret == left)
-        {
+    while (len != 0 ) {
+        int ret = ::send(fd_, ptr, len, flags);
+        if (ret == 0 || ret == len)
             return( total + ret );
-        }
 
-        if (ret == -1)
-        {
-            if (errno == EINTR)
+        if (ret == -1) {
+            if (errno == EINTR) {
                 continue;
-            else
+            } else if (ecode==EAGAIN) {
+                return total;
+            } else {
                 return (-1); //error
+            }
         }
-
-        left -= ret;
-        ptr += ret;
-        total += ret;
+        len -= ret; ptr += ret; total += ret;
     }
 
     return (total);
-
 }
-ssize_t CSocket::recv( Buffer & buffer, int flags)
+ssize_t CSocket::recv(char *data, size_t len, int flags)
 {
-	ssize_t total(0);
-    size_t left = buffer.size();
-    char *ptr = (char *)buffer.data();
+    ssize_t total(0);
+    int ret (0), ecode(0);
+    char * ptr = data;
 
-    while (left != 0) {
-        int ret = ::recv(fd_, ptr, left, flags);
-
-        if (ret == 0 || ret == left)
-        {
+    while (len != 0) {
+        ret = ::recv(fd_, ptr, len, flags);
+        if (ret == 0 || ret == len)
             return( total + ret );
-        }
 
-        if (ret == -1)
-        {
-            if (errno == EINTR)
+        if (ret == -1) {
+            ecode = errno;
+            if (ecode == EINTR) {
                 continue;
-            else
+            } else if (ecode==EAGAIN) { //11, resource temporarily unavailable
+               return total;
+            } else {
+                INFO("*****Recv msg: errno!=EINTR: err=" << ecode << "; str=" <<  strerror(ecode) );
                 return (-1); //error
+            }
         }
-
-        left -= ret;
-        ptr += ret;
-        total += ret;
+        len -= ret;  ptr += ret; total += ret;
     }
 
     return (total);
@@ -121,13 +123,13 @@ DSocket::DSocket(int domain, bool sctp)
 //ssize_t sendto(int socket, const void *message, size_t length, int flags, const struct sockaddr *dest_addr, socklen_t dest_len);
 //ssize_t recvfrom(int socket, void *restrict buffer, size_t length, int flags, struct sockaddr * address, socklen_t * address_len);
 
-ssize_t DSocket::send(const Buffer & buffer, const Address & addr, int flags)
+ssize_t DSocket::send(const char * data, size_t len, const Address & addr, int flags)
 {
 	ssize_t cnt(0);
 	int errNum(0);
 
 	//while (true) {
-		cnt = ::sendto(fd_, (const void *)buffer.data(), buffer.size(), flags, addr.addr(), addr.length());
+		cnt = ::sendto(fd_, (const void *)data, len, flags, addr.addr(), addr.length());
 		if (cnt != -1) return cnt;
 		errNum = errno;
 		//if(errNum == ENOMEM || errNum == ENOSR)
@@ -135,21 +137,21 @@ ssize_t DSocket::send(const Buffer & buffer, const Address & addr, int flags)
 	//}
 }
 
-ssize_t DSocket::recv(Buffer & buffer, Address & addr, int flags)
+ssize_t DSocket::recv(char * data, size_t len, Address & addr, int flags)
 {
 	ssize_t cnt(0);
 	int errNum(0);
 	socklen_t addrLen = addr.maxLength();
 
-	//while (true) {
-		cnt = ::recvfrom(fd_, (void *) buffer.data(), buffer.size(), flags, addr.addr(), &addrLen);
+	//while (true) { // one time read
+		cnt = ::recvfrom(fd_, data, len, flags, addr.addr(), &addrLen);
 		if (cnt != -1) return cnt;
 		errNum = errno;
+		return -1;
 		//if(errNum == ENOMEM || errNum == ENOSR)
 		//other throw exception
 	//}
 }
-
 
 RSocket::RSocket(int domain, int protocol)
 : Socket(domain, SOCK_RAW, protocol)
