4b53327ac76ed7ff49b60a0e9738c37fe5481326
SharedSmally <SharedSmally@gmail.com>
2019-10-18 23:10:55 -0400

    Update Macro.h

--- cxx/net1/Macro.h
+++ cxx/net1/Macro.h
@@ -4,7 +4,12 @@
 #include <iostream>
 
 #ifndef INFO
-#define INFO(msg) std::cout << msg << std::endl;
+#define INFO(msg) std::cout << __FILE__ << "@" << __LINE__ << ": " << msg << std::endl;
+#endif
+
+#ifndef ERROR
+#define ERROR(msg) std::cout << __FILE__ << "@" << __LINE__ << ": errno=" << errno << ": "<< msg << std::endl;
 #endif
 
 #endif
+
