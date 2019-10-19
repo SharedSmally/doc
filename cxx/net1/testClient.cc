f09e66b91dd0a738eae27179ce84d99b65335bc4
SharedSmally <SharedSmally@gmail.com>
2019-10-18 23:15:21 -0400

    Update testClient.cc

--- cxx/net1/testClient.cc
+++ cxx/net1/testClient.cc
@@ -1,4 +1,5 @@
 #include <Utils.h>
+#include <Macro.h>
 #include <EpollMux.h>
 
 #include <iostream>
@@ -8,16 +9,36 @@ using namespace std;
 
 class MyChannelListener : public ChannelListener
 {
+	typedef IPv4Address addr_type;
+    typedef ChannelObj<ClientSocket<addr_type> > ClientChannel;
+    typedef std::shared_ptr<ClientChannel> ClientChannelPtr;
+
 public:
 	MyChannelListener(const IPv4Address & svcAddr)
-	: svcAddr_(svcAddr) {}
+	: svcAddr_(svcAddr), mtx_(nullptr) {}
     virtual ~MyChannelListener() {}
-
+    void setMultiplexer(Multiplexer & mtx) {
+        mtx_ = &mtx;
+    }
 protected:
     virtual int _onRead(ChannelPtr & channel, bool prio)
     {
     	if (channel) {
     		INFO("MyChannelListener onRead: id="<<channel->id());
+			ClientChannelPtr ptr = std::dynamic_pointer_cast<ClientChannel>(channel);
+			if (!ptr->sock().isConnected()) {
+				INFO("MyChannelListener onRead: connected to server");
+				ptr->sock().setConnected(true);
+				mtx_->manage(channel);
+				Buffer msg("hello from client");
+				ptr->sock().send(msg);
+				mtx_->manage(channel);
+			} else {
+				char msg[10]; memset(msg, '\0',10);
+				ptr->sock().recv(msg, 10);
+				INFO("**Recv msg:" << msg);
+				mtx_->manage(channel);
+			}
     	} else {
     		INFO("MyChannelListener onRead: channel is null");
     	}
@@ -25,7 +46,12 @@ protected:
     virtual int _onWrite(ChannelPtr & channel)
     {
     	if (channel) {
+    		Buffer msg("hello from client");
     		INFO("MyChannelListener _onWrite: id="<<channel->id());
+    		sleep(5);
+    		ClientChannelPtr ptr = std::dynamic_pointer_cast<ClientChannel>(channel);
+    		ptr->sock().send(msg);
+    		mtx_->manage(channel);
     	} else {
     		INFO("MyChannelListener _onWrite: channel is null");
     	}
@@ -34,6 +60,7 @@ protected:
     {
     	if (channel) {
     		INFO("MyChannelListener _onHup: id="<<channel->id());
+    		mtx_->unmanage(channel);
     	} else {
     		INFO("MyChannelListener _onHup: channel is null");
     	}
@@ -42,6 +69,7 @@ protected:
     {
     	if (channel) {
     		INFO("MyChannelListener _onError: id="<<channel->id());
+    		mtx_->unmanage(channel);
     	} else {
     		INFO("MyChannelListener _onError: channel is null");
     	}
@@ -49,20 +77,22 @@ protected:
 
 protected:
     IPv4Address svcAddr_;
+    Multiplexer * mtx_;
 };
 
 int main()
 {
-    IPv4Address addr("10.32.119.152"); // no port
-    IPv4Address svrAddr("10.32.119.152", 9999);
+    IPv4Address addr("127.0.0.1"); // no port
+    IPv4Address svrAddr("127.0.0.1", 9999);
     auto channel = clientChannel(addr, svrAddr);
 
     INFO("clientApp: server addr="<<svrAddr <<"; fd=" << channel->fd());
     MyChannelListener listener(addr);
 
     EpollMux epoll(listener);
+    listener.setMultiplexer(epoll);
     epoll.start();
     epoll.manage(channel);
-    sleep(10);
+    sleep(20);
     epoll.stop();
 }
