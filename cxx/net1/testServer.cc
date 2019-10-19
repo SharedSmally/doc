ac0fd25a1ff03393c95beea089d4dbcd329c1415
SharedSmally <SharedSmally@gmail.com>
2019-10-18 23:16:24 -0400

    Update testServer.cc

--- cxx/net1/testServer.cc
+++ cxx/net1/testServer.cc
@@ -1,4 +1,5 @@
 #include <Utils.h>
+#include <Macro.h>
 #include <EpollMux.h>
 
 #include <iostream>
@@ -9,23 +10,48 @@ using namespace std;
 class MyChannelListener : public ChannelListener
 {
 public:
-	MyChannelListener(const IPv4Address & svcAddr)
-	: svcAddr_(svcAddr) {}
+	typedef IPv4Address addr_type;
+	MyChannelListener(const addr_type & svcAddr)
+	: svcAddr_(svcAddr), mtx_(nullptr) {}
     virtual ~MyChannelListener() {}
+    void setMultiplexer(Multiplexer & mtx) {
+        mtx_ = &mtx;
+    }
+    typedef ChannelObj<ServerSocket<addr_type> > ServerChannel;
+    typedef ChannelObj<Connection<addr_type> > ConnectionChannel;
+    typedef std::shared_ptr<ServerChannel> ServerChannelPtr;
+    typedef std::shared_ptr<ConnectionChannel> ConnectionChannelPtr;
 
 protected:
     virtual int _onRead(ChannelPtr & channel, bool prio)
     {
-    	if (channel) {
-    		INFO("MyChannelListener onRead: id="<<channel->id());
+    	if (channel) { //std::shared_ptr<ChannelObj<ServerSocket<ADDR> > >
+    		INFO("MyChannelListener onRead: id="<<channel->id()<<"; serverf="<< channel->server());
+    		if (channel->server()) {
+    			ServerChannelPtr ptr = std::dynamic_pointer_cast<ServerChannel>(channel);
+    			ConnectionChannelPtr conn = ::connectChannel<addr_type>(ptr);
+    			mtx_->manage(conn);
+    		} else {
+    			ConnectionChannelPtr ptr = std::dynamic_pointer_cast<ConnectionChannel>(channel);
+				char msg[20]; memset(msg, '\0',20);
+				ptr->sock().recv(msg, 20);
+				INFO("**Recv msg:" << msg);
+				mtx_->manage(channel);
+    		}
     	} else {
     		INFO("MyChannelListener onRead: channel is null");
     	}
     }
     virtual int _onWrite(ChannelPtr & channel)
     {
+    	Buffer msg("hello");
     	if (channel) {
     		INFO("MyChannelListener _onWrite: id="<<channel->id());
+    		sleep(5);
+    		ConnectionChannelPtr ptr = std::dynamic_pointer_cast<ConnectionChannel>(channel);
+    		ptr->sock().send(msg);
+    		mtx_->manage(channel);
+    		INFO("**Send msg:" << msg);
     	} else {
     		INFO("MyChannelListener _onWrite: channel is null");
     	}
@@ -34,6 +60,7 @@ protected:
     {
     	if (channel) {
     		INFO("MyChannelListener _onHup: id="<<channel->id());
+    		mtx_->unmanage(channel);
     	} else {
     		INFO("MyChannelListener _onHup: channel is null");
     	}
@@ -42,25 +69,29 @@ protected:
     {
     	if (channel) {
     		INFO("MyChannelListener _onError: id="<<channel->id());
+    		mtx_->unmanage(channel);
     	} else {
     		INFO("MyChannelListener _onError: channel is null");
     	}
     }
 
 protected:
-    IPv4Address svcAddr_;
+    addr_type svcAddr_;
+    Multiplexer * mtx_;
 };
 
 
 
 int main()
 {
-    IPv4Address addr("10.32.119.152", 9999);
+    IPv4Address addr("127.0.0.1", 9999);
     auto channel = serverChannel(addr);
     INFO("server addr="<<addr <<"; fd=" << channel->fd());
     MyChannelListener listener(addr);
 
     EpollMux epoll(listener);
+    listener.setMultiplexer(epoll);
+
     epoll.start();
     epoll.manage(channel);
     epoll.join();
