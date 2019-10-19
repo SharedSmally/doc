c5ebc76e7733a62f9da7db903076375903e5e15b
SharedSmally <SharedSmally@gmail.com>
2019-10-18 23:14:39 -0400

    Update Socket.h

--- cxx/net1/Socket.h
+++ cxx/net1/Socket.h
@@ -1,20 +1,19 @@
-#ifndef SOCKET_H
-#define SOCKET_H
+#ifndef CPW_NET_SOCKET_H
+#define CPW_NET_SOCKET_H
 
 #include <string>
 #include <sys/socket.h>
 
-//Only for IPAddress.
 #include <Address.h>
 
-typedef std::string Buffer;
+typedef std::string Buffer; // not work as Buffer
 
 class Socket {
 	const static int BAD_FD;
 
 public:
 	const static int DEFAULT_BACKLOG;
-	Socket(int fd = BAD_FD) : fd_(fd) { } ;
+	Socket(int fd = BAD_FD) : fd_(fd) { _nonblock(); } ;
 	Socket(int domain, int type, int protocol=0);
 	virtual ~Socket();
 	int fd()  { return fd_;}
@@ -25,6 +24,8 @@ protected:
 	int _connect(const Address & addr);
 	int _listen(int backlog);
 	int _accept(Address & addr);
+
+	int _nonblock();
 
 protected:
 	int fd_;
@@ -41,19 +42,20 @@ public:
 	virtual ~CSocket()
 	{};
 
-	ssize_t send(const Buffer & buffer, int flags=0);
-	ssize_t recv(Buffer & buffer, int flags=0);
+	ssize_t send(const Buffer & data, int flags=0) { return send(data.data(), data.size(), flags); }
+	ssize_t send(const char * data, size_t len, int flags=0);
+	ssize_t recv(char * data, size_t len, int flags=0);
 };
 
 //local address
 class DSocket : public Socket {
 public:
-	//DSocket(int fd ) : Socket(fd) { } ;
 	DSocket(int domain, bool sctp = false);
 	virtual ~DSocket(){};
 
-	ssize_t send(const Buffer & buffer, const Address & addr, int flags=0);
-	ssize_t recv(Buffer & buffer, Address & addr, int flags=0);
+	ssize_t send(const Buffer & buffer, const Address & addr, int flags=0) { return send(buffer.data(),buffer.size(),addr, flags);}
+	ssize_t send(const char * data, size_t len, const Address & addr, int flags=0);
+	ssize_t recv(char * data, size_t len, Address & addr, int flags=0);
 };
 
 //raw_socket = socket(AF_INET, SOCK_RAW, protocol);
@@ -81,120 +83,4 @@ public:
 //ssize_t sendmsg(int sockfd, const struct msghdr *msg, int flags);
 //ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags);
 
-
-/*
-connection:type=SOCK_STREAM/0; SOCK_STREAM/IPPROTO_SCTP (one-to-one style)
-A typical server has the following sequences :
-     1. socket()
-     2. bind()
-     3. listen()
-     4. accept()
-        4a: accept() blocks until return a new fd to send()/recv()
-     5. close()
-A typical client has the following sequences:
-     1. socket()
-     2. connect()
-     2a. send()/recv()
-     3. close()
-*/
-template <typename ADDR>
-class Connection : public CSocket {
-public:
-	Connection(const ADDR & localAddr, const ADDR & remoteAddr, int fd)
-	: localAddr_(localAddr), remoteAddr_(remoteAddr), Socket(fd)
-	{ };
-	virtual ~Connection(){};
-
-	ssize_t send(const Buffer & buffer, int flags=0);
-	ssize_t recv(Buffer & buffer, int flags=0);
-
-	const ADDR & remoteAddr() const { return remoteAddr_;}
-	const ADDR & localAddr() const  { return localAddr_; }
-	const ADDR & serverAddr() const { return localAddr_; }
-
-protected:
-	ADDR remoteAddr_; // remote
-	ADDR localAddr_;  // local server address
-};
-
-
-
-//local and remote address
-template <typename ADDR>
-class ClientSocket : public CSocket {
-public:
-	ClientSocket(const ADDR & addr, const ADDR & svrAddr, bool sctp=false)
-		: remoteAddr_(svrAddr), localAddr_(addr),
-		  CSocket(addr.domain(), sctp)
-	{
-	}
-	virtual ~ClientSocket() { };
-
-	const ADDR & remoteAddr() const { return remoteAddr_;}
-	const ADDR & localAddr() const  { return localAddr_; }
-	const ADDR & serverAddr() const { return remoteAddr_; }
-
-	int connect(bool bind=false)
-	{
-		int ret(0);
-		if (bind)
-		{
-			ret = _bind(localAddr_);
-		}
-		ret = _connect(remoteAddr_);
-		return ret;
-	}
-
-protected:
-	ADDR remoteAddr_;// remote server address
-	ADDR localAddr_; // local address
-};
-
-//local address
-template <typename ADDR>
-class ServerSocket : public Socket {
-
-public:
-	ServerSocket(const ADDR & addr, bool sctp=false)
-	: localAddr_(addr), Socket(addr.domain(),
-			sctp ? SOCK_STREAM : SOCK_STREAM,
-			sctp ? IPPROTO_SCTP : 0)
-	{}
-	virtual ~ServerSocket(){};
-
-	const ADDR & localAddr() const  { return localAddr_; }
-	const ADDR & serverAddr() const { return localAddr_; }
-
-	int listen(int backlog=DEFAULT_BACKLOG)
-	{
-		int ret = _bind(localAddr_);
-		ret = _listen(backlog);
-		return ret;
-	}
-
-	// blocked to return connection
-	int accept(ADDR & remoteAddr)
-	{
-		return _accept(remoteAddr);
-	}
-
-protected:
-	ADDR localAddr_; // local address
-};
-
-/*
- connectionless socket: SOCK_DGRAM/0; SOCK_SEQPACKET/IPPROTO_SCTP
- A typical server has the following sequences:
-     1. socket()
-     2. bind()
-     3. listen()
-     4. recvmsg()
-     5. sendmsg()
-     6. close()
- A typical client has the following sequences:
-     1. socket()
-     2. sendmsg()
-     3. recvmsg()
-     4. close()
- */
 #endif
