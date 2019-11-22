0b1911fdc8a12f85541c374c28a846e3efae4936
SharedSmally <SharedSmally@gmail.com>
2019-11-22 13:04:08 -0500

    Update k8sapi.mk

--- k8s/k8sapi.mk
+++ k8s/k8sapi.mk
@@ -1,10 +1,25 @@
 K8S_VERSION=1.16
 K8S_URL=https://kubernetes.io/docs/reference/generated/kubernetes-api/v${K8S_VERSION}/
 
-main: k8sapiREST.xml k8sapiRes.xml
+main: k8sapiREST.xml k8sapiRes.xml k8sapiEx.xml k8sapiIndex.xml
 
-k8sapiREST.xml:k8sapi.xml
+k8sapiIndex.xml:k8sapiIndex1.xml 
+	xsl k8sapiIndex.xsl $< $@
+
+k8sapiIndex1.xml:k8sapiIndex0.xml 
+	xsl k8sapiIndex1.xsl $< $@
+
+k8sapiIndex0.xml:k8s.xml 
+	xsl k8sapiIndex0.xsl $< $@
+
+k8sapiEx.xml:k8s.xml 
+	xsl k8sapiEx.xsl $< $@
+
+k8sapiREST.xml:k8sapiREST0.xml
 	xsl k8sapiREST.xsl $< $@
+
+k8sapiREST0.xml:k8sapi.xml
+	xsl k8sapiREST0.xsl $< $@
 
 k8sapiRes.xml:k8sapi.xml
 	xsl k8sapiRes.xsl $< $@
@@ -27,6 +42,7 @@ k8s.html:
 
 clean:
 	${RM} k8sapiREST.xml
+	${RM} k8sapiREST0.xml
 #	${RM} k8sapiRes.xml
 
 cleanall: clean
